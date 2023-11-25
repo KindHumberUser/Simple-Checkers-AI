b7e45468ad31143799d7783044a6de70068880f4
Sagar <rsagar@vt.edu>
2023-11-24 23:12:41 -0500

    Incomplete

--- game.py
+++ game.py
@@ -4,5 +4,11 @@ class game:
     def __init__(self):
         self.winner = None
 
-    def check_piece(self, board)
+    def check_piece(self, board):
+        #Number of pieces
+        red = 0
+        black = 0
+
+        for x,y in range(board):
+
     
