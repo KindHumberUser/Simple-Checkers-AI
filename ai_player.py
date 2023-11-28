cb90fb0b6bcbc3350efbffe1cccd7488bbff83a8
Sagar <rsagar@vt.edu>
2023-11-28 16:27:18 -0500

    Improved AI Evaluation

--- ai_player.py
+++ ai_player.py
@@ -44,10 +44,7 @@ class AIPlayer:
         depth = 4  # Depth can be adjusted for difficulty
 
         valid_moves = self.get_all_valid_moves(board)
-        capturing_moves = [move for move in valid_moves if self.is_capturing_move(move)]
-
-        if capturing_moves:
-            valid_moves = capturing_moves
+        
 
         for move in valid_moves:
             score = self.minimax(move, board, depth, False, alpha, beta)
@@ -55,8 +52,6 @@ class AIPlayer:
                 best_score = score
                 best_move = move
 
-        if capturing_moves:
-            print("Forced")
 
         return best_move
 
@@ -86,19 +81,83 @@ class AIPlayer:
 
     def evaluate_board(self, board):
         score = 0
+
+        # Evaluate piece values and positions
         for tile in board.tile_list:
             piece = tile.occupying_piece
             if piece:
-                # Assign scores to pieces: Pawn = 1, King = 1.5
                 piece_value = 1.5 if piece.notation == 'k' else 1
                 if piece.color == self.color:
                     score += piece_value
                 else:
                     score -= piece_value
 
-                # Maybe we can add more logic here for evaluating position of the pieces as well
+                # Add positional evaluation
+                if piece.color == self.color:
+                    if piece.notation == 'p':
+                        # Bonus for pawns controlling the center
+                        score += 0.1 * (3 - abs(3 - tile.x))
+                    # elif piece.notation == 'k':
+                    #     # Penalty for the king being exposed
+                    #     if self.is_king_in_check(self.color, board):
+                    #         score -= 0.5
+                else:
+                    if piece.notation == 'p':
+                        # Bonus for opponent's pawns controlling the center
+                        score -= 0.1 * (3 - abs(3 - tile.x))
+                    # elif piece.notation == 'k':
+                    #     # Penalty for the opponent's king being exposed
+                    #     if board.is_king_in_check(self.get_opponent_color()):
+                    #         score += 0.5
+
+        # Evaluate pawn structure
+        for x in range(1, 7):
+            for y in range(1, 7):
+                tile = board.get_tile_from_pos((x,y))
+                if tile.occupying_piece and tile.occupying_piece.notation == 'p':
+                    # Bonus for connected pawns
+                    connected_pawns = self.count_connected_pawns(board, x, y)
+                    score += 0.2 * connected_pawns
 
         return score
+    
+    # def is_king_in_check(self, color, board):
+    #     king_position = None
+
+    #     # Find the position of the king of the specified color
+    #     for tile in board.tile_list:
+    #         piece = tile.occupying_piece
+    #         if piece and piece.notation == 'k' and piece.color == color:
+    #             king_position = (tile.x, tile.y)
+    #             break
+
+    #     if king_position is None:
+    #         # King not found, something is wrong with the board state
+    #         raise ValueError("King not found on the board")
+
+    #     # Check if any opponent's piece threatens the king
+    #     for tile in board.tile_list:
+    #         piece = tile.occupying_piece
+    #         if piece and piece.color != color:
+    #             valid_moves = self.get_valid_moves(tile.x, tile.y, board)
+    #             if king_position in valid_moves:
+    #                 return True
+
+    #     return False
+
+
+    def count_connected_pawns(self, board, x, y):
+        count = 0
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                if dx == 0 and dy == 0:
+                    continue
+                nx, ny = x + dx, y + dy
+                if 1 <= nx <= 6 and 1 <= ny <= 6:
+                    neighbor_tile = board.get_tile_from_pos((nx,ny))
+                    if neighbor_tile.occupying_piece and neighbor_tile.occupying_piece.notation == 'p':
+                        count += 1
+        return count
 
     def get_all_valid_moves(self, board):
         valid_moves = []
